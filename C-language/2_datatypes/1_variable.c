bac1f261d637f68e9df8466d9004267275da6ed7
shivam24s <shivamshrivastav8055@gmail.com>
2026-02-06 15:56:35 +0530

    datatypes code added

---
+++ C-language/2_datatypes/1_variable.c
@@ -0,0 +1,25 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+
+    // variable declaration
+
+    int num;
+
+    // variable initialization
+
+    num = 10;
+
+    // rules
+    // you can't declare variable in same scope
+
+    // int num = 10;
+
+    // printf("%d", num);
+
+
+    return 0;
+}
