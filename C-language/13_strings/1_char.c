83855677d4900b024436b5f2fba7e2d8069ce545
shivam24s <shivamshrivastav8055@gmail.com>
2026-03-17 16:01:07 +0530

    strings

---
+++ C-language/13_strings/1_char.c
@@ -0,0 +1,27 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+
+    char letter = 'A';
+
+    printf("%c", letter);
+
+    printf("\n%d\n", letter);
+
+    char lastLetter = 'Z';
+
+    printf("\n%c",lastLetter);
+
+    printf("\n%d",lastLetter);
+
+    char smallLetter = 'a';
+
+    printf("\n%c",smallLetter);
+
+    printf("\n%d",smallLetter);
+
+    return 0;
+}
