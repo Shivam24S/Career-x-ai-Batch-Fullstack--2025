5fed6f469cca2b56222ae8bb9ca49c75fffc45fc
shivam24s <shivamshrivastav8055@gmail.com>
2026-05-14 15:58:56 +0530

    deep copy and shallow copy

---
+++ JAVASCRIPT/02_Js_intro_basic_concept/08_operators.js
@@ -0,0 +1,12 @@
+// 1. arithmetic operators
+
+let a = 10;
+
+let b = 5;
+
+console.log("addition", a + b);
+console.log("subtraction", a - b);
+console.log("multiplication", a * b);
+console.log("division", a / b);
+console.log("modulus", a % b);
+console.log("exponential", a ** b);
