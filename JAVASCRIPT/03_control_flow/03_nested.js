048fab6f26c458e45644fdd3011f2427f5108faf
shivam24s <shivamshrivastav8055@gmail.com>
2026-05-18 16:03:44 +0530

    control statement code added

---
+++ JAVASCRIPT/03_control_flow/03_nested.js
@@ -0,0 +1,13 @@
+let age = 15;
+
+let drivingLicense = false;
+
+if (age >= 18) {
+  if (drivingLicense === true) {
+    console.log("you can drive vehicle");
+  } else {
+    console.log("you have to register for driving license");
+  }
+} else {
+  console.log("you are not eligible for driving car");
+}
