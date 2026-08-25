69695745ecfb9bc4d2a53c1f579013b3bc21838f
shivam24s <shivamshrivastav8055@gmail.com>
2026-08-25 16:02:21 +0530

    crud operation added

--- Backend/06_express_crud/server.js
+++ Backend/06_express_crud/server.js
@@ -3,6 +3,8 @@ import HttpError from "./middleware/HttpError.js";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json("hello from server");
 });
@@ -26,7 +28,11 @@ app.get("/taskList", (req, res, next) => {
       return res.json({ message: "no task data found" });
     }
 
-    res.json({success:true, message: "task data fetched successfully", taskList });
+    res.json({
+      success: true,
+      message: "task data fetched successfully",
+      taskList,
+    });
   } catch (error) {
     next(new HttpError(error.message, 500));
   }
@@ -42,11 +48,101 @@ app.get("/taskList/:id", (req, res, next) => {
       return res.json({ message: "no task data found" });
     }
 
-    res.status(200).json({success:true,message:"task found with this id",task})
-
-
+    res
+      .status(200)
+      .json({ success: true, message: "task found with this id", task });
   } catch (error) {
-      next(new HttpError(error.message, 500));
+    next(new HttpError(error.message, 500));
+  }
+});
+
+app.post("/task/add", (req, res, next) => {
+  try {
+    const { task, description } = req.body;
+
+    console.log("task", task);
+    console.log("description", description);
+
+    if (task === undefined && description === undefined) {
+      return next(new HttpError("task and description data is required", 400));
+    }
+
+    const newTask = {
+      id: new Date().getTime(),
+      task: task,
+      description: description,
+    };
+
+    taskList.push(newTask);
+
+    res.status(201).json({
+      success: true,
+      message: "task data added successfully",
+      newTask,
+    });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+});
+
+app.delete("/task/:id", (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const index = taskList.findIndex((t) => t.id === Number(id));
+
+    if (index === -1) {
+      return next(new HttpError("task not found with this id", 404));
+    }
+
+    taskList.splice(index, 1);
+
+    return res
+      .status(200)
+      .json({ success: true, message: "task deleted successfully", taskList });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+});
+
+app.patch("/task/:id", (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const updateTask = taskList.find((t) => t.id === Number(id));
+
+    if (updateTask === undefined) {
+      return next(new HttpError("task not found with this id", 404));
+    }
+
+    const { task, description } = req.body;
+
+    if (task) {
+      updateTask.task = task;
+    }
+
+    if (description) {
+      updateTask.description = description;
+    }
+
+    if (task === undefined && description === undefined) {
+      return next(
+        new HttpError(
+          "for updating task data task or description is required",
+          400,
+        ),
+      );
+    }
+
+    return res
+      .status(200)
+      .json({
+        success: true,
+        message: "task data updated successfully",
+        updateTask,
+      });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
   }
 });
 
