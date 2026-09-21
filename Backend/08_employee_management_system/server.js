d9658c5b90d9f4fe8f2029c5075c8e1782c9345a
shivam24s <shivamshrivastav8055@gmail.com>
2026-09-21 16:25:21 +0530

    initial setup

---
+++ Backend/08_employee_management_system/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import HttpError from "./middlewares/HttpError.js";
+
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "hello from server" });
+});
+
+app.use((req, res, next) => {
+  return next(new HttpError("requested route not found", 404));
+});
+
+app.use((error, req, res, next) => {
+  if (res.headersSent) {
+    return next(error);
+  }
+
+  res
+    .status(error.statusCode || 500)
+    .json({ message: error.message || "internal server error" });
+});
+
+const port = 5000;
