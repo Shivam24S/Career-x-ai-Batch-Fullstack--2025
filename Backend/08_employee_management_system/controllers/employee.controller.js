1666de891d8a076fe4f43eaced75c30ee5a19d49
shivam24s <shivamshrivastav8055@gmail.com>
2026-09-23 15:58:36 +0530

    read and delete code added

--- Backend/08_employee_management_system/controllers/employee.controller.js
+++ Backend/08_employee_management_system/controllers/employee.controller.js
@@ -1,6 +1,7 @@
 import EmployeeModel from "../models/Employee.model.js";
 
 import HttpError from "../middlewares/HttpError.js";
+import mongoose from "mongoose";
 
 const add = async (req, res, next) => {
   try {
@@ -14,7 +15,7 @@ const add = async (req, res, next) => {
       mobileNo,
     });
 
-    await  newEmployee.save();
+    await newEmployee.save();
 
     res.status(201).json({
       success: true,
@@ -26,4 +27,61 @@ const add = async (req, res, next) => {
   }
 };
 
-export default add;
+const allEmployeeData = async (req, res, next) => {
+  try {
+    const employeeList = await EmployeeModel.find({});
+
+    if (employeeList.length === 0) {
+      return res
+        .status(200)
+        .json({ success: true, message: "no employee data found" });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "employee list fetched successfully",
+      total: employeeList.length,
+      employeeList,
+    });
+  } catch (error) {
+    return next(new HttpError(error.message, 500));
+  }
+};
+
+const getEmpById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const validId = mongoose.isValidObjectId(id);
+
+    if (!validId) {
+      return next(new HttpError("employee with this id not found", 404));
+    }
+
+    const employee = await EmployeeModel.findById(id);
+
+    res.status(200).json({ success: true, message: "emp found", employee });
+  } catch (error) {
+    return next(new HttpError(error.message, 500));
+  }
+};
+
+const deleteById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const employee = await EmployeeModel.findByIdAndDelete(id);
+
+    if (!employee) {
+      return next(new HttpError("failed to delete employee", 500));
+    }
+
+    res
+      .status(200)
+      .json({ success: true, message: "employee delete successfully" });
+  } catch (error) {
+    return next(new HttpError(error.message, 500));
+  }
+};
+
+export default { add, allEmployeeData, getEmpById, deleteById };
--- Backend/08_employee_management_system/routes/employee.routes.js
+++ Backend/08_employee_management_system/routes/employee.routes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import add from "../controllers/employee.controller.js";
+import EmployeeController from "../controllers/employee.controller.js";
 
 const router = express.Router();
 
-router.post("/add", add);
+router.post("/add", EmployeeController.add);
+
+router.get("/allEmployeeData", EmployeeController.allEmployeeData);
+
+router.get("/:id", EmployeeController.getEmpById);
+
+router.delete("/:id", EmployeeController.deleteById);
 
 export default router;
